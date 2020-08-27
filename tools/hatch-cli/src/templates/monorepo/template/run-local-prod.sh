9023eade6d267e2373eeee87d36f2f7554dc11e9
Kilty McGowan <kiltyj@gmail.com>
2020-08-26 21:59:10 -0700

    Add docker download URL to production script error message

--- tools/hatch-cli/src/templates/monorepo/template/run-local-prod.sh
+++ tools/hatch-cli/src/templates/monorepo/template/run-local-prod.sh
@@ -3,6 +3,7 @@ set -e
 if ! command -v docker-compose &> /dev/null
 then
   >&2 echo "Please make sure Docker is installed, with docker-compose in your PATH"
+  >&2 echo "Download it here: https://download.docker.com/mac/stable/Docker.dmg"
   exit 1
 fi
 docker-compose up --remove-orphans --build --no-start

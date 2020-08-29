94ca3b9a78879ea3ccf20dc595679be25dd8e9e0
Kilty McGowan <kiltyj@gmail.com>
2020-08-28 17:51:53 -0700

    Improve logging format

--- libraries/hatch-util/src/ConsoleLogger.ts
+++ libraries/hatch-util/src/ConsoleLogger.ts
@@ -1,17 +1,8 @@
 import {Logger} from './Logger';
 
 export class ConsoleLogger implements Logger {
-    public debug = console.debug.bind(console, this.createPrefix('debug'));
-    public info = console.info.bind(console, this.createPrefix('info'));
-    public warn = console.warn.bind(console, this.createPrefix('warn'));
-    public error = console.error.bind(console, this.createPrefix('error'));
-
-    // Note: evaluated at time of construction, not time of logging
-    private createPrefix(level: string) {
-        return `[${this.label}] [${level}]:`;
-    }
-
-    constructor(private readonly label: string) {
-        this.label = label;
-    }
+  public debug = console.debug.bind(console);
+  public info = console.info.bind(console);
+  public warn = console.warn.bind(console);
+  public error = console.error.bind(console);
 }

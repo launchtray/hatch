3bcbbdb66070ba972e7ab278a260c50fe148424d
Kilty McGowan <kiltyj@gmail.com>
2022-10-04 16:58:16 -0700

    Add command for API generation, fix 'dependency' generation option

--- libraries/hatch-client-sdk/src/util.ts
+++ libraries/hatch-client-sdk/src/util.ts
@@ -1,6 +1,5 @@
 import {spawnSync} from 'child_process';
 import path from 'path';
-import tmp from 'tmp';
 import fs from 'fs-extra';
 
 export const createClientSDKByInputSpec = async (inputSpec: string) => {
@@ -40,28 +39,6 @@ export const createClientSDKByInputSpec = async (inputSpec: string) => {
 };
 
 export const createClientSDKByDependency = async (dependencyName: string) => {
-  const fileResult = tmp.fileSync();
-  const tempFilePath = fileResult.name;
-  const cleanUp = fileResult.removeCallback;
-
-  try {
-    const serverExec = path.resolve(process.cwd(), 'node_modules', dependencyName, 'build', 'server.js');
-    const env = Object.create(process.env);
-    env.PRINT_API_SPEC_ONLY = 'true';
-    const printSpecCmd = spawnSync('node', [serverExec], {
-      encoding: 'utf8',
-      env,
-      maxBuffer: 10 * 1024 * 1024,
-    });
-    if (printSpecCmd.error != null) {
-      // eslint-disable-next-line no-console -- intentional stdout
-      console.log(printSpecCmd.stdout);
-      throw new Error(printSpecCmd.error.message);
-    }
-    const inputSpec = printSpecCmd.stdout;
-    fs.writeFileSync(tempFilePath, inputSpec);
-    await createClientSDKByInputSpec(tempFilePath);
-  } finally {
-    cleanUp();
-  }
+  const inputSpec = path.resolve(process.cwd(), 'node_modules', dependencyName, 'dist', 'api.json');
+  await createClientSDKByInputSpec(inputSpec);
 };

815a3705475749ab17c01f37ff6ed73c309a4fec
Kilty McGowan <kiltyj@gmail.com>
2022-12-12 13:28:15 -0800

    Update story template

--- tools/hatch-cli/src/templates/story/template.stories.tsx
+++ tools/hatch-cli/src/templates/story/template.stories.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import HATCH_CLI_TEMPLATE_VAR_moduleName from './HATCH_CLI_TEMPLATE_VAR_moduleName';
 
+// See https://storybook.js.org/docs/react/writing-stories/introduction for a guide on writing stories
+
 export default {
   title: 'HATCH_CLI_TEMPLATE_VAR_moduleName',
   component: HATCH_CLI_TEMPLATE_VAR_moduleName,
 };
 
-export const Basic = () => <HATCH_CLI_TEMPLATE_VAR_moduleName/>;
+export const Basic = {};

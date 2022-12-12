c1101c0834de1e8feca03164fee621dc030154d9
Kilty McGowan <kiltyj@gmail.com>
2022-12-12 14:18:50 -0800

    Fix lint issue for story template

--- tools/hatch-cli/src/templates/story/template.stories.tsx
+++ tools/hatch-cli/src/templates/story/template.stories.tsx
@@ -1,3 +1,6 @@
+// The next two lines (and this one) can be removed when JSX is added
+/* eslint-disable react/jsx-filename-extension */
+/* eslint-disable-next-line @typescript-eslint/no-unused-vars */
 import React from 'react';
 import HATCH_CLI_TEMPLATE_VAR_moduleName from './HATCH_CLI_TEMPLATE_VAR_moduleName';
 

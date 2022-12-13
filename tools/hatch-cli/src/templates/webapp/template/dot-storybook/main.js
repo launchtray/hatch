65dba840c0936a4c27061a88a21fb57c32fee884
Kilty McGowan <kiltyj@gmail.com>
2022-12-12 18:28:16 -0800

    Remove extra typescript config for storybook

--- tools/hatch-cli/src/templates/webapp/template/dot-storybook/main.js
+++ tools/hatch-cli/src/templates/webapp/template/dot-storybook/main.js
@@ -13,14 +13,6 @@ module.exports = {
   ],
   framework: '@storybook/react',
   webpackFinal: config => {
-    config.module.rules.push({
-      include: [path.resolve(__dirname, '../src')],
-      test: /\.(ts|tsx)$/,
-      use: [{
-        loader: require.resolve('ts-loader'),
-      }],
-    });
-    config.resolve.extensions.push('.ts', '.tsx');
     return razzleConfig.patchWebpackConfig(config);
   },
 };
